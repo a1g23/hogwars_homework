226814917623f1b2901ad609e12f5fa3606dae05
Alex Greenberg <acgreenberg2@gmail.com>
2023-12-01 15:00:49 -0500

    Commit 3 - I added list elements. That code didn't seem very dry. I wonder if there is something I could do about it in the Hungry for More section

--- app.js
+++ app.js
@@ -30,6 +30,59 @@
     
     $container.append($h4Wand)
     $h4Wand.text("Super Special Wand")
+
+// adding ul with attribute of storage = trunk
+
+const $ulTrunk = $("<ul>")
+$ulTrunk.attr("storage", "trunk")
+console.log($ulTrunk)
+
+$container.append($ulTrunk)
+
+// butter beer
+
+const $liButterBeer = $("<li>")
+$liButterBeer.text("Butter Beer")
+$ulTrunk.append($liButterBeer)
+
+
+// invisibility cloak (add a class of secret)
+
+const $liCloak = $("<li>")
+$liCloak.addClass("secret")
+$liCloak.text("Invisibility Cloak")
+$ulTrunk.append($liCloak)
+
+// magic map (add a class of secret)
+
+const $liMap = $("<li>")
+$liMap.addClass("secret")
+$liMap.text("Magic Map")
+$ulTrunk.append($liMap)
+
+// time turner (add a class of secret)
+
+const $liTimeTurn = $("<li>")
+$liTimeTurn.addClass("secret")
+$liTimeTurn.text("Time Turner")
+$ulTrunk.append($liTimeTurn)
+
+
+// leash (for your pet, be sure to give this list element the same class as you gave your pet)
+
+const $liLeash = $("<li>")
+$liLeash.addClass("dog")
+$liLeash.text("Leash")
+$ulTrunk.append($liLeash)
+
+// Bertie Bott's Every Flavor [Jelly] Beans.
+
+const $liBertie = $("<li>")
+$liBertie.text("Bertie Bott's Every Flavor [Jelly] Beans")
+$ulTrunk.append($liBertie)
+
+
+
 });
 
 
