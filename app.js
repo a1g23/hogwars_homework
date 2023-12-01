75dd1c4596fd6aee5b4b7bf2be12366a027bbafa
Alex Greenberg <acgreenberg2@gmail.com>
2023-12-01 14:40:40 -0500

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- app.js
+++ app.js
@@ -5,15 +5,33 @@
 
     const $h1 = $("<h1>")
     console.log($h1)
-    $h1.text = "Hogwarts"
-    console.log($h1)
-    $container.append($h1.text)
+    $container.append($h1)
+    $h1.text("Hogwarts")
+ 
+    const $h2 = $("<h2>")
+    console.log($h2)
+    $container.append($h2)
+    $h2.text("Alex Greenberg")
+
+    const $h3 = $("<h3>")
+    console.log($h3)
+    $container.append($h3)
+    $h3.text("Gryffindor")
+
+    const $h4 = $("<h4>")
+    console.log($h4)
+    $h4.addClass("dog")
+    console.log($h4)
+    $container.append($h4)
+    $h4.text("Nellie")
+
+    const $h4Wand = $("<h4>")
+    console.log($h4Wand)
+    
+    $container.append($h4Wand)
+    $h4Wand.text("Super Special Wand")
 });
-// Query for your div with the id of container and set it to a variable named $container
-// console.log $container
-// Create an <h1> element and set it to a variable called $h1 and console log it
-// Add some text inside the h1 element. Example text: 'Hogwarts'
-// Why isn't your $h1 it appearing on your page? I havent appended it yet
+
 
 
   
